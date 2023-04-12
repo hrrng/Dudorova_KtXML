fda7bfb45cba16e9cd5c0241093b64b77dd3ee24
hrrng <dudorovauliana@gmail.com>
2023-04-12 19:41:23 +0200

    atempt3

--- app/src/main/java/com/dudorova/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/dudorova/myfirstapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.text.InputType
 import android.widget.Toast
+import android.content.Context
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,45 +18,61 @@ class MainActivity : AppCompatActivity() {
         val editTextEmail = findViewById<EditText>(R.id.EnterEmail)
         val editTextPassword1 = findViewById<EditText>(R.id.EnterPassword1)
         val editTextPassword2 = findViewById<EditText>(R.id.EnterPassword2)
-        val textView = findViewById<TextView>(R.id.WithEmail)
-        val textView2 = findViewById<TextView>(R.id.WithNumber)
-        button.setOnClickListener{
-            val myText = editTextEmail.text.toString()
-            if (myText.contains("@"))
-            else {
-                Toast.makeText(this, "Введите корректный email c @",Toast.LENGTH_LONG).show()
-            }
-            val myText2 = editTextEmail.text.toString()
-            if (myText2.contains("+"))
-            else {
-                Toast.makeText(this, "Введите корректный номер телефона начинающийся с +",Toast.LENGTH_LONG).show()
-            }
-            val myText3 = editTextPassword1.text.toString().length.toInt()
-            if (myText3.coerceAtLeast(8))
-            else {
-            Toast.makeText(this, "Минимальная длина пароля 8 символов",Toast.LENGTH_LONG).show()
-        }
-            val myText4 = editTextPassword1.text.toString()
-            if (myText4.equals(editTextPassword2.text.toString()))
-            else {
-                Toast.makeText(this, "Пароли не совпадают",Toast.LENGTH_LONG).show()
-            }
-        }
-        textView.setOnClickListener {
-            textView.setTextColor(resources.getColor(R.color.purple_200))
+        val textView2 = findViewById<TextView>(R.id.WithEmail)
+        val textView = findViewById<TextView>(R.id.WithNumber)
+
+        textView2.setOnClickListener {
+            textView2.setTextColor(resources.getColor(R.color.green_A700))
+            textView.setTextColor(resources.getColor(com.google.android.material.R.color.material_blue_grey_800))
             editTextEmail.setHint("Введите email")
             editTextEmail.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
         }
-        textView2.setOnClickListener {
-            textView2.setTextColor(resources.getColor(R.color.purple_200))
+        textView.setOnClickListener {
+            textView.setTextColor(resources.getColor(R.color.green_A700))
+            textView2.setTextColor(resources.getColor(com.google.android.material.R.color.material_blue_grey_800))
             editTextEmail.setHint("Введите номер")
             editTextEmail.inputType = InputType.TYPE_CLASS_PHONE
         }
-        editTextPassword1.setOnClickListener{
 
-        }
-        editTextPassword2.setOnClickListener{
+        button.setOnClickListener {
+            val myText = editTextEmail.text.toString()
 
+            if (editTextEmail.inputType == InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS) {
+                if (myText.isNotEmpty()
+                    && myText.contains("@")
+                )
+                else {
+                    Toast.makeText(this, "Введите корректный email c @", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+            }
+
+            if(editTextEmail.inputType == InputType.TYPE_CLASS_PHONE) {
+                if (myText.isNotEmpty()
+                    && myText.contains("+")
+                )
+                else {
+                    Toast.makeText(this, "Введите корректный номер телефона начинающийся с +", Toast.LENGTH_LONG).show()
+                    return@setOnClickListener
+                }
+            }
+            val myText3 = editTextPassword1.text.toString()
+            if (myText3.isNotEmpty()
+                && myText3.length > 8)
+            else {
+            Toast.makeText(this, "Минимальная длина пароля 8 символов",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
         }
+            val myText4 = editTextPassword1.text.toString()
+            if (myText4.isNotEmpty()
+                && myText4.equals(editTextPassword2.text.toString()))
+            else {
+                Toast.makeText(this, "Пароли не совпадают",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+        }
+
+
+
         }
     }
